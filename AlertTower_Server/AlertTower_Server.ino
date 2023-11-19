f192eeb609745ad01a616a9548a0dc33cfd35a6a
d3vmeh <devxmehra@gmail.com>
2023-11-19 08:39:43 -0800

    Create AlertTower_Server.ino

---
+++ AlertTower_Server/AlertTower_Server.ino
@@ -0,0 +1,23 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <string.h>
+#include "DHT.h"
+#include <math.h>
+
+
+
+//RF95 Setup
+#define RF95_FREQ 868.0
+RH_RF95 driver(8, 3); // Adafruit Feather M0 with RFM95 
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
